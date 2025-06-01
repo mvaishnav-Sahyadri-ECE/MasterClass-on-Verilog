aced79b4a5722b40b47b17bfa66ef1ce859bbfa7
mvaishnav-Sahyadri-ECE <mvaishnavofficial2@gmail.com>
2025-06-01 17:42:11 +0530

    Update up_down_counter.v

--- Assignment_4_UpDown_Counter/up_down_counter.v
+++ Assignment_4_UpDown_Counter/up_down_counter.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.06.2025 17:23:47
-// Design Name: 
-// Module Name: up_down_counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module up_down_counter (
     input clk,
