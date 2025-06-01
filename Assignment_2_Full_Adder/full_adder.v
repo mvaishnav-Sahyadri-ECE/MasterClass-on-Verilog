fa266eb64a9a2570ef0a32b53fe50ba60707803f
mvaishnav-Sahyadri-ECE <mvaishnavofficial2@gmail.com>
2025-06-01 17:44:22 +0530

    Update full_adder.v

--- Assignment_2_Full_Adder/full_adder.v
+++ Assignment_2_Full_Adder/full_adder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.06.2025 17:10:38
-// Design Name: 
-// Module Name: full_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module full_adder (
     input a, b, cin,
