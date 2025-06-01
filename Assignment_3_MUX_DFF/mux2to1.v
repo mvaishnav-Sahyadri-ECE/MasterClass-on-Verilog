8f8cfa7abd3cbec2de7702252806f165b71b62a8
mvaishnav-Sahyadri-ECE <mvaishnavofficial2@gmail.com>
2025-06-01 17:43:27 +0530

    Update mux2to1.v

--- Assignment_3_MUX_DFF/mux2to1.v
+++ Assignment_3_MUX_DFF/mux2to1.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.06.2025 17:17:07
-// Design Name: 
-// Module Name: mux2to1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mux2to1 (
     input wire a, b,
